b0a19dab84a5a23c8f5cf2116361e5c059f570c5
aqr444 <aqr444@empal.com>
2020-03-20 10:57:37 +0900

    requirements.txt add uwsgi

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_restful import Api, reqparse
 from flask_jwt import JWT
@@ -6,18 +7,16 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-# from db import db
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://guest:1111@192.168.3.110/soogeup_api?charset=utf8'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://guest:1111@192.168.3.110/soogeup_api?charset=utf8'
+# 환경변수 DATABASE_URL 이 없으면 디폴트(SQLite3-data.db) 사용
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'aqr4'
 api = Api(app)
 
 jwt = JWT(app, authenticate, identity)   # /auth
-
-items = []
 
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
@@ -26,5 +25,11 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
+    from db import db
     db.init_app(app)
-    app.run(port=5000, debug=True)
+
+    @app.before_first_request # 위 config에서 설정한 DB를 생성합니다.  강의 109.  근데 안되는데 ?
+    def create_tables():
+        db.create_all()
+
+    app.run(host='192.168.1.101', port=5000, debug=True)
